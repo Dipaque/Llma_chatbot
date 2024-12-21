ab0ed1ade3fd6457de0861d92ec66d30056311a2
Deepak <rdeepak@isupportz.com>
2024-12-21 14:23:18 +0530

    removed cookies

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,7 @@
 import { NextResponse, NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-    const path = request.nextUrl.pathname;
-    const email = request.cookies.get("email")?.value;
-    const userName = request.cookies.get("userName")?.value;
-    const photoURL = request.cookies.get("photoURL")?.value;
+   
 
     if(request.nextUrl.pathname === '/' ){
         return NextResponse.redirect(new URL('/login', request.url));
